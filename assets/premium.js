4257cff207df04659f2c93fd5f5e9d58eb0ef7f7
maite-terapia <tessatacbu@gmail.com>
2026-09-08 12:34:32 +0200

    Fix About introduction source markup

--- assets/premium.js
+++ assets/premium.js
@@ -105,7 +105,7 @@
   /* About page: use the portrait supplied by the client beside the existing introduction text. */
   if (body.classList.contains('floreix-about') || (document.querySelector('.elementor-element[data-id="bcb05fa"]') && document.querySelector('.elementor-element[data-id="855140f"]'))) {
     const oldIntro = document.querySelector('.elementor-element[data-id="bcb05fa"]');
-    const introWidget = oldIntro?.querySelector('.elementor-element[data-id="5bf1d4d"] .elementor-widget-container');
+    const introWidget = oldIntro?.querySelector('.elementor-element[data-id="5bf1d4d"] .elementor-widget-container') || oldIntro?.querySelector('.elementor-element[data-id="5bf1d4d"]');
     const oldPortrait = document.querySelector('.elementor-element[data-id="fe1a261"]');
     const missionBlock = document.querySelector('.elementor-element[data-id="855140f"]');
 
